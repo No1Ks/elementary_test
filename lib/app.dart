a8647e30f36aa6263d61b047605a119ca6fbf6c4
ya.savelyev <ya.savelyev@madbrains.ru>
2024-10-23 13:20:40 +0400

    decomposed

---
+++ lib/app.dart
@@ -0,0 +1,19 @@
+import 'package:elementary_test/home_page/home_page.dart';
+import 'package:flutter/material.dart';
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Elementary test',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const HomePage(title: 'Elementary test'),
+    );
+  }
+}
